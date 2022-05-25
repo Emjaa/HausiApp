0dccd362fe114f843712fca658a3036edb6bd0f8
ole <ole@eggersmann.hamburg>
2022-05-26 01:26:16 +0200

    added small clock to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {map, share, Subscription, timer} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'HausiApp';
   showFiller = false;
+  time = new Date();
+  rxTime = new Date();
+  intervalId = 0;
+  subscription: Subscription = new Subscription();
+
+  ngOnInit(): void {
+    // Using Basic Interval
+    this.intervalId = setInterval(() => {
+      this.time = new Date();
+    }, 1000);
+
+    // Using RxJS Timer
+    this.subscription = timer(0, 1000)
+      .pipe(
+        map(() => new Date()),
+        share()
+      )
+      .subscribe(time => {
+        this.rxTime = time;
+      });
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
   formatLabel(value: number) {
     if (value >= 1000) {
       return Math.round(value / 1000) + 'k';
     }
     return value;
   }
-
 }
